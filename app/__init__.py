900f5b5ac5dd6760afb6d692585018e97c81205d
Taylor Elledge <taylorelledge@hotmail.com>
2018-02-06 16:49:42 -0800

    First branch w/ templating and preliminary ldap configs

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,23 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.config['SQLALCHEMY_DB_URI'] = 'sqlite:////temp/test.db'
+app.config['WTF_CSRF_SECRET_KEY'] = 'fuck the celtics'
+app.config['LDAP_PROVIDER_URL'] = 'insert LDAP server here'
+app.config['LDAP_PROTOCOL_VERSION'] = 3
+db = SQLAlchemy(app)
+
+app.secret_key = 'fuck the giants'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+#from my_app.auth.views import auth
+#app.register_blueprint(auth) 
 
 from app import routes
